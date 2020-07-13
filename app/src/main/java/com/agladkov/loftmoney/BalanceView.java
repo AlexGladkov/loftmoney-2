acf0a7c4325daba7abfe09a8ff83ee55726cfb7f
Alex Gladkov <alexgladkov@icloud.com>
2020-07-13 19:07:11 +0300

    custom drawable

---
+++ app/src/main/java/com/agladkov/loftmoney/BalanceView.java
@@ -0,0 +1,34 @@
+package com.agladkov.loftmoney;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class BalanceView extends View {
+
+    public BalanceView(Context context) {
+        super(context);
+    }
+
+    public BalanceView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BalanceView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public BalanceView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        
+    }
+}
--- app/src/main/java/com/agladkov/loftmoney/SecondActivity.java
+++ app/src/main/java/com/agladkov/loftmoney/SecondActivity.java
@@ -1,18 +1,39 @@
 package com.agladkov.loftmoney;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerTabStrip;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SecondActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        int costId = getIntent().getIntExtra("COST_ID", 0);
-        Log.e("TAG", "Cost Id = " + costId);
+        mViewPager = findViewById(R.id.vpCosts);
+        mTabLayout = findViewById(R.id.tlCosts);
+        List<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(CostsFragment.getInstance());
+        fragmentList.add(CostsFragment.getInstance());
+        fragmentList.add(DiagramFragment.getInstance());
+        CostsPagerAdapter costsPagerAdapter = new CostsPagerAdapter(fragmentList, getSupportFragmentManager(), 0);
+        mViewPager.setAdapter(costsPagerAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 }
