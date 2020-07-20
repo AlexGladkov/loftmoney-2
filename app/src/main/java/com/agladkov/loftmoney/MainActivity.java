7cf732b19bbb5cf77fd8c9f943832b01a8d35338
Alex Gladkov <aleksey.gladkov@leroymerlin.ru>
2020-07-20 21:28:37 +0300

    Flow list end

--- app/src/main/java/com/agladkov/loftmoney/MainActivity.java
+++ app/src/main/java/com/agladkov/loftmoney/MainActivity.java
@@ -1,26 +1,79 @@
 package com.agladkov.loftmoney;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.os.Handler;
+
+import com.agladkov.loftmoney.cells.money.MoneyAdapter;
+import com.agladkov.loftmoney.cells.money.MoneyAdapterClick;
+import com.agladkov.loftmoney.cells.money.MoneyCellModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    MoneyAdapter moneyAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btnClick).setOnClickListener(new View.OnClickListener() {
+        recyclerView = findViewById(R.id.costsRecyclerView);
+        moneyAdapter = new MoneyAdapter();
+        moneyAdapter.setMoneyAdapterClick(new MoneyAdapterClick() {
             @Override
-            public void onClick(View v) {
-                Intent newActivity = new Intent(getApplicationContext(), SecondActivity.class);
-                newActivity.putExtra("COST_ID", 1);
-                startActivity(newActivity);
+            public void onMoneyClick(MoneyCellModel moneyCellModel) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onValueClick(String value) {
+
             }
         });
+
+        recyclerView.setAdapter(moneyAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(),
+            LinearLayoutManager.VERTICAL, false));
+
+        moneyAdapter.setData(generateExpenses());
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                moneyAdapter.addData(generateIncomes());
+                moneyAdapter.addData(generateIncomes());
+                moneyAdapter.addData(generateIncomes());
+                moneyAdapter.addData(generateIncomes());
+            }
+        }, 3000);
+    }
+
+    private List<MoneyCellModel> generateExpenses() {
+        List<MoneyCellModel> moneyCellModels = new ArrayList<>();
+        moneyCellModels.add(new MoneyCellModel("Молоко", "70 ₽", R.color.expenseColor));
+        moneyCellModels.add(new MoneyCellModel("Зубная щётка", "70 ₽", R.color.expenseColor));
+        moneyCellModels.add(new MoneyCellModel("Сковородка с антипригарным покрытием",
+            "1670 ₽", R.color.expenseColor));
+
+        return moneyCellModels;
+    }
+
+    private List<MoneyCellModel> generateIncomes() {
+        List<MoneyCellModel> moneyCellModels = new ArrayList<>();
+        moneyCellModels.add(new MoneyCellModel("Зарплата.Июнь", "70000 ₽", R.color.incomeColor));
+        moneyCellModels.add(new MoneyCellModel("Премия", "7000 ₽", R.color.incomeColor));
+        moneyCellModels.add(new MoneyCellModel("Олег наконец-то вернул долг",
+            "300000 ₽", R.color.incomeColor));
+
+        return moneyCellModels;
     }
 }
---
+++ app/src/main/java/com/agladkov/loftmoney/cells/money/MoneyAdapterClick.java
@@ -0,0 +1,6 @@
+package com.agladkov.loftmoney.cells.money;
+
+public interface MoneyAdapterClick {
+    void onMoneyClick(MoneyCellModel moneyCellModel);
+    void onValueClick(String value);
+}
